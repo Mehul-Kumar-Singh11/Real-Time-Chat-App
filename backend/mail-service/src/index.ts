f3c8bd75f42799b02645324f524988b025b8861a
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-08-02 21:27:22 +0530

    feat: added RabbitMq consumer file  in mail-service index.ts  to handle OTP emails

--- backend/mail-service/src/index.ts
+++ backend/mail-service/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import { startSendOtpConsumer } from "./consumer.js";
 
 dotenv.config();
+
+startSendOtpConsumer();
 
 const app = express();
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-  console.log(`✅ Server is listening at PORT ${port}`);
+  console.log(`Server is listening at PORT ${port}`);
 });
