5f8b18dec6f4c9ad55394c81b381afb2a68b22ac
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-08-02 19:29:25 +0530

    feat: setup Redis  and RabbitMq in user-service

--- backend/user-service/src/config/db.ts
+++ backend/user-service/src/config/db.ts
@@ -1,22 +1,19 @@
 import mongoose from "mongoose";
 
-const connectDb = async() => {
-    const url = process.env.MONGO_URI;
+const connectDb = async () => {
+  const url = process.env.MONGO_URI;
 
-    if(!url) {
-        throw new Error('MONGO_URI is not defined in environment variables.')
-    }
+  if (!url) {
+    throw new Error("MONGO_URI is not defined in environment variables.");
+  }
 
-    try{
-        await mongoose.connect(url, {
-            dbName: "ChatAppMicroservice"
-        })
-        console.log("Connected to MongoDb");
-    }
-    catch(error) {
-        console.error("Error while connecting to Mongodb: ", error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(url);
+    console.log("✅ Connected to MongoDb");
+  } catch (error) {
+    console.error("❌ Error while connecting to Mongodb: ", error);
+    process.exit(1);
+  }
+};
 
 export default connectDb;
--- backend/user-service/src/index.ts
+++ backend/user-service/src/index.ts
@@ -1,15 +1,35 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import userRoutes from "./routes/user.js";
+import { connectRabbitMQ } from "./config/rabbitMq.js";
 
 dotenv.config();
 
 connectDb();
 
+connectRabbitMQ();
+
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient
+  .connect()
+  .then(() => {
+    console.log("✅ Redis Connected");
+  })
+  .catch((error) => {
+    console.error("❌ Error while connecting to redis: ", error);
+  });
+
 const app = express();
+
+app.use("/api/v1", userRoutes);
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running on PORT ${port}`)
+  console.log(`Server is running on PORT ${port}`);
 });
