e53dca272f8b286df79e503e3febc72b9806c20c
Mehul Kumar Singh <mehulkumarsingh391@gmail.com>
2025-08-04 16:38:01 +0530

    feat: implement chat-service, including create-chat, send-msg, and retrieve-chat-msgs endpoints.

---
+++ backend/chat-service/src/middlewares/isAuth.ts
@@ -0,0 +1,53 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Document } from "mongoose";
+
+interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+}
+
+export interface AuthenticatedRequest extends Request {
+  user?: IUser | null;
+}
+
+export const isAuth = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      res.status(401).json({
+        message: "Please login - No Auth Header",
+      });
+      return;
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decodedValue = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string
+    ) as JwtPayload;
+
+    if (!decodedValue || !decodedValue.user) {
+      res.status(401).json({
+        message: "Invalid token",
+      });
+      return;
+    }
+
+    req.user = decodedValue.user;
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.status(401).json({
+      message: "Please login - JWT error",
+    });
+  }
+};
