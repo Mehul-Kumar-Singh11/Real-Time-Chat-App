10fc075aaccce1932d081f8d628e110d213f683e
Mehul-Kumar-Singh11 <mehulkumarsingh391@gmail.com>
2025-09-07 12:25:46 +0530

    feat: add AppContext logic, toast, and cors

--- backend/chat-service/src/index.ts
+++ backend/chat-service/src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import chatRoutes from "./routes/chat.js";
+import cors from "cors";
 
 dotenv.config();
 
 connectDb();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json())
 
--- frontend/src/app/verify/page.tsx
+++ frontend/src/app/verify/page.tsx
@@ -1,69 +1,13 @@
-"use client";
 import React from "react";
-import { ArrowRight, Lock } from "lucide-react";
-import { useSearchParams } from "next/navigation";
-import { Loader2 } from "lucide-react";
+import VerifyOTP from "@/components/VerifyOTP";
+import { Suspense } from "react";
+import Loading from "../../components/Loading";
 
 const VerifyPage = () => {
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const searchParams = useSearchParams();
-
-  const email: string = searchParams.get("email") || "";
-
-  const handleSubmit = async () => {};
   return (
-    <div className="min-h-screen bg-gray-900 flex items-center justify-center p-4">
-      <div className="max-w-md w-full">
-        <div className="bg-gray-800 border border-gray-700 rounded-lg p-8">
-          <div className="text-center mb-8">
-            <div className="mx-auto w-20 h-20 bg-blue-600 rounded-lg flex items-center justify-center mb-6">
-              <Lock size={40} className="text-white" />
-            </div>
-            <h1 className="text-4xl font-bold text-white mb-3">
-              Verify Your Email
-            </h1>
-            <p className="text-gray-300 text-lg">Sent a 6-digit code to</p>
-            <p className="text-blue-400 font-medium">{email}</p>
-          </div>
-
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-300 mb-2"
-              >
-                Email Address
-              </label>
-              <input
-                type="email"
-                id="email"
-                className="w-full px-4 py-4 bg-gray-700 border border-gray-600 rounded-lg text-white placeholder-gray-400"
-                placeholder="Enter your email address"
-                // value={email}
-                // onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-full bg-blue-600 text-white  py-4 px-6 rounded-lg font-semibold hover:bg-blue-700 disabled:opacity-50 disabled:cursor-not-allowed disabled={loading}"
-            >
-              {loading ? (
-                <div className="flex items-center justify-center gap-2">
-                  <Loader2 className="w-5 h-5" />
-                  Verifying...
-                </div>
-              ) : (
-                <div className="flex items-center justify-center gap-2">
-                  <span> Verify </span>
-                  <ArrowRight className="w-5 h-5" />
-                </div>
-              )}
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <VerifyOTP />
+    </Suspense>
   );
 };
 
